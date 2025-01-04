76ae35ca58bdecbdf29474b8804c76ca8878671d
Your GitHub Username <your.email@example.com>
2025-01-05 02:16:56 +0900

    Update next.config.js and package.json for improved PWA support and build process. Enabled PWA in development mode, optimized image handling, and adjusted Webpack configuration. Modified build script in package.json to streamline the build process. These changes enhance application performance and user experience.

--- next.config.js
+++ next.config.js
@@ -1,21 +1,23 @@
 /** @type {import('next').NextConfig} */
-
 const withPWA = require('next-pwa')({
   dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
   register: true,
   skipWaiting: true,
 });
 
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: false,
-  },
-  eslint: {
-    ignoreDuringBuilds: false,
-  },
+  output: 'export',
   images: {
-    domains: ['lh3.googleusercontent.com'],
-  }
+    unoptimized: true,
+  },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+    };
+    return config;
+  },
 };
 
 module.exports = withPWA(nextConfig); 
